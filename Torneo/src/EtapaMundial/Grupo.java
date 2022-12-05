5fc45dd8b4446e167ba5f50482418dbd3fac2a0a
YeisonGP <115736572+YeisonGP@users.noreply.github.com>
2022-12-05 14:56:12 -0300

    test

--- Torneo/src/EtapaMundial/Grupo.java
+++ Torneo/src/EtapaMundial/Grupo.java
@@ -1,5 +1,19 @@
 package EtapaMundial;
 
+import Partido.Partido;
+
 public class Grupo extends EtapaMundial {
 
+	public Grupo(String descriopcion, Partido[] partido) {
+		super(descriopcion, partido);
+		// TODO Auto-generated constructor stub
+	}
+
+	private String EquiposQueAvanzan;
+
+	@Override
+	public String toString() {
+		return "Grupo [EquiposQueAvanzan=" + EquiposQueAvanzan + "]";
+	}
+	
 }
---
+++ Torneo/src/Partido/Partido.java
@@ -0,0 +1,5 @@
+package Partido;
+
+public class Partido {
+
+}
