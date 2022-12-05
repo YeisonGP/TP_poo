86cfbec8ed2dfc444c21f06d255edd245c7d5ed4
YeisonGP <115736572+YeisonGP@users.noreply.github.com>
2022-12-04 22:15:26 -0300

    2

--- TP_poo/src/Garage.java
+++ TP_poo/src/Garage.java
@@ -1,4 +1,13 @@
 
 public class Garage {
 
+	private int capacidad = 25;
+	private String direccion;
+	private int precio = 500;
+	public boolean ver_capacidad=true;
+	public boolean ingresar=true;
+	public String mandar_aviso;
+	
+	
+	
 }
