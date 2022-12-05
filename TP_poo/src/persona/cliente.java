a06879ed1a747fb4dc033081c1733f67a02ad5aa
YeisonGP <115736572+YeisonGP@users.noreply.github.com>
2022-12-05 10:40:35 -0300

    persona

--- TP_poo/src/persona/cliente.java
+++ TP_poo/src/persona/cliente.java
@@ -1,6 +1,34 @@
 package persona;
 
 class cliente {
+	
+	private String nombreCliente;
+	private String dniCliente;
+	private String patenteCliente;
+	public cliente(String nombreCliente, String dniCliente, String patenteCliente) {
+		super();
+		this.nombreCliente = nombreCliente;
+		this.dniCliente = dniCliente;
+		this.patenteCliente = patenteCliente;
+	}
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
+	public String getDniCliente() {
+		return dniCliente;
+	}
+	public void setDniCliente(String dniCliente) {
+		this.dniCliente = dniCliente;
+	}
+	public String getPatenteCliente() {
+		return patenteCliente;
+	}
+	public void setPatenteCliente(String patenteCliente) {
+		this.patenteCliente = patenteCliente;
+	}
 
 	
 }
--- TP_poo/src/persona/empleado.java
+++ TP_poo/src/persona/empleado.java
@@ -1,5 +1,36 @@
 package persona;
-
 public class empleado {
+	
 
+	private String dni_empleado;
+	private boolean empleado= false;
+	
+	public empleado(String dni_empleado, boolean empleado) {
+		super();
+		this.dni_empleado = dni_empleado;
+		this.empleado = empleado;
+	}
+
+	public String getDni_empleado() {
+		return dni_empleado;
+	}
+
+	public void setDni_empleado(String dni_empleado) {
+		this.dni_empleado = dni_empleado;
+	}
+
+	public boolean isEmpleado() {
+		return empleado;
+	}
+
+	public void setEmpleado(boolean empleado) {
+		this.empleado = empleado;
+	}
+
+	@Override
+	public String toString() {
+		return "empleado [dni_empleado=" + dni_empleado + ", empleado=" + empleado + "]";
+	}
+	
+	
 }
