716867c56f859241fabb9d61bf2c55ec54cb40ee
YeisonGP <115736572+YeisonGP@users.noreply.github.com>
2022-12-05 19:06:32 -0300

    llave y grupo

--- TP_poo/Torneo/src/Torneo/Grupo.java
+++ TP_poo/Torneo/src/Torneo/Grupo.java
@@ -1,5 +1,31 @@
 package Torneo;
+import java.util.ArrayList;
+public class Grupo extends EtapaMudial{
 
-public class Grupo {
+	private ArrayList<Equipo> equiposQueAvanzan;   
+  
+    public Grupo() {
+        super();
+        
+    }
+    
+    
+	public Grupo(String descripcionEtapa) {
+		super();
+		setDescripcionEtapa(descripcionEtapa);
+		this.equiposQueAvanzan = new ArrayList<Equipo>();
+	}
+	
 
+    @Override
+    public ArrayList<Equipo> getEquiposQueAvanzan(){
+      
+        return super.getEquiposQueAvanzan();
+    }
+
+
+	@Override
+	public String toString() {
+		return "Grupo [equiposQueAvanzan=" + equiposQueAvanzan + "]";
+	} 
 }
--- TP_poo/Torneo/src/Torneo/Llave.java
+++ TP_poo/Torneo/src/Torneo/Llave.java
@@ -1,5 +1,27 @@
 package Torneo;
+import java.util.ArrayList;
 
-public class Llave {
+public class Llave extends EtapaMudial {
+	
+	private ArrayList<Equipo> equiposQueAvanzan;
+	
+	public Llave() {
+	}
+	
+	public Llave (String descripcionEtapa) {
+        this.equiposQueAvanzan = new ArrayList<Equipo>();
+    }
 
+	@Override
+	public ArrayList<Equipo> getEquiposQueAvanzan(){
+		return super.getEquiposQueAvanzan();
+	}
+
+	@Override
+	public String toString() {
+		return "Llave [equiposQueAvanzan=" + equiposQueAvanzan + "]";
+	}
+	
+	
+	
 }
