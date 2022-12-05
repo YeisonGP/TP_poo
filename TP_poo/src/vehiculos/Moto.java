bd72d73d7eddc573d4383870fcde44fad7151c12
YeisonGP <115736572+YeisonGP@users.noreply.github.com>
2022-12-05 10:57:55 -0300

    moto

--- TP_poo/src/vehiculos/Moto.java
+++ TP_poo/src/vehiculos/Moto.java
@@ -1,42 +1,44 @@
 package vehiculos;
-public class Moto extends Vehiculo{
+public class Moto{
 
 	private String marca;
-	private int cilindradas;
-	
-	
-	
-	public Moto(String motor, int combustible, boolean encendido, String matricula, int ruedas, boolean desplazarse) {
-		super(motor, combustible, encendido, matricula, ruedas, desplazarse);
-		// TODO Auto-generated constructor stub
+	private int cc;
+	private int km;
+	private int cantRuedas;
+	public Moto(String marca, int cc, int km, int cantRuedas) {
+		super();
+		this.marca = marca;
+		this.cc = cc;
+		this.km = km;
+		this.cantRuedas = cantRuedas;
 	}
-
-
-
-	
-	
-	
-	
 	public String getMarca() {
 		return marca;
 	}
-
-
-
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
-
-
-
-	public int getCilindradas() {
-		return cilindradas;
+	public int getCc() {
+		return cc;
 	}
-
-
-
-	public void setCilindradas(int cilindradas) {
-		this.cilindradas = cilindradas;
+	public void setCc(int cc) {
+		this.cc = cc;
+	}
+	public int getKm() {
+		return km;
+	}
+	public void setKm(int km) {
+		this.km = km;
+	}
+	public int getCantRuedas() {
+		return cantRuedas;
+	}
+	public void setCantRuedas(int cantRuedas) {
+		this.cantRuedas = cantRuedas;
+	}
+	@Override
+	public String toString() {
+		return "Moto [marca=" + marca + ", cc=" + cc + ", km=" + km + ", cantRuedas=" + cantRuedas + "]";
 	}
 
 	
